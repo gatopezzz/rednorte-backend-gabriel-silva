22142eadd03349b640258ba8e3277f13857e0f89
gatopezzz <gab.silvaj@duocuc.cl>
2026-04-24 00:36:04 -0400

    init: configuracion inicial del ecosistema de microservicios

---
+++ src/main/java/com/rednorte/rednorte_parent/RednorteParentApplication.java
@@ -0,0 +1,13 @@
+package com.rednorte.rednorte_parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RednorteParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RednorteParentApplication.class, args);
+	}
+
+}
